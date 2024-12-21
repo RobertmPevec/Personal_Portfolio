ddcfd486564cff09f6ff0ac1a20a91cd4e0e520b
RobertmPevec <robertmpevec@gmail.com>
2024-12-21 02:07:18 -0500

    first stage

---
+++ src/data/info.ts
@@ -0,0 +1,84 @@
+export const info = {
+  baseUrl: "https://robertpevec.com",
+  name: "Robert Pevec",
+  jobDescription: "Software Engineer",
+  about: `I’m a Software Engineer based in Waterloo, Ontario,
+   with a passion for creating new things and solving complex problems.
+    My skills include Python, C, HTML, CSS,
+     and JavaScript. With a strong foundation in mathematics
+      and a curiosity for learning, I enjoy building solutions,
+       analyzing information, and approaching challenges with an
+        analytical mindset.
+  `,
+
+  experience: [
+    {
+      name: "Full-Stack Developer Intern",
+      location: "Sampler | Toronto, ON",
+      startDate: "Jan 2024",
+      endDate: "Apr 2024",
+      description: [
+        "✓ Improved website load speeds by 20% using React.js optimizations.",
+        "✓ Developed backend logic in Django for the product sampling dashboard, supporting 5,000+ daily users.",
+        "✓ Monitored and optimized database performance with tools like pgAdmin and query analysis."
+      ],
+    },
+
+    {
+      name: "Apprentice Carpenter",
+      location: "Local 1256 | Sarnia, ON",
+      startDate: "Sep 2021",
+      endDate: "Jan 2024",
+      description: [
+        "✓ Helped run crews to ensure efficient project completion.",
+        "✓ Worked with clients to ensure satisfaction and project alignment with expectations.",
+        "✓ Maintained tools and materials, ensuring projects stayed on schedule and within budget."
+      ],
+    },
+
+    {
+      name: "Apprentice Roofer",
+      location: "Imperial Roofing | Sarnia, ON",
+      startDate: "Aug 2020",
+      endDate: "Jun 2021",
+      description: [
+        "✓ Installed and repaired various roofing systems, sheet metal, and flat roofs.",
+        "✓ Resolved on-site issues and made adjustments to meet client expectations.",
+        "✓ Completed Working at heights and First Aid and CPR Certification.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Wilfrid Laurier University",
+      location: "Waterloo, Ontario",
+      startDate: "2023",
+      endDate: "2027",
+      description: [
+        "<strong>Bachelor of Science in Computer Science and Mathematics</strong>",
+        "✓ Co-authored an academic paper on bias in AI using machine learning under a professor",
+        "✓ Team Captain of the university chess club",
+        "✓ Strong foundation in calculus and linear algebra for analytical problem-solving."
+      ],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
