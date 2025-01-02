283646c9db70dd398618347cdaf4cbc3a094fe48
RobertmPevec <robertmpevec@gmail.com>
2025-01-01 23:15:53 -0500

    update with blog and contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://robertpevec.com",
   integrations: [
     tailwind(),
     sitemap({
