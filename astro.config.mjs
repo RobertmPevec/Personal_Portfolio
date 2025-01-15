d8c7dd5bf018882f9dbcb3a657999f16721b8565
RobertmPevec <robertmpevec@gmail.com>
2025-01-14 22:58:46 -0500

    rrr
    rrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://robertpevec.com', // Your site URL
-  outDir: './output-test', // Change the output directory here
-  integrations: [
-    tailwind(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-      entryLimit: 10000,
-    }),
-    robotsTxt(),
-  ],
-  base: '/', // Base path for the site
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: undefined, // Prevent unwanted chunk splitting
-        },
-      },
-    },
-  },
+  site: 'https://robertpevec.com', // Replace with your site URL
+  outDir: './dist', // Ensure output is directed to 'dist'
 });
